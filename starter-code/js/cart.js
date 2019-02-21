834dbb7f551b4b60f562937f985e515388e8ed86
Mary Cimakasky <MaryCimakasky@Marys-MBP.localdomain>
2019-02-21 15:47:47 -0800

    large part of cart hashed out - working on local storage now

--- starter-code/js/cart.js
+++ starter-code/js/cart.js
@@ -21,29 +21,51 @@ function renderCart() {
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
   var tr = document.getElementsByClassName('tr');
-  tr.innerhtml('');
+  tr.innerhtml = '';
 }
 
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
-function showCart() {
+function showCart() { 
 
   // TODO: Find the table body
+  var tbody = document.getElementsByClassName('tbody');
 
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  for (var i = 0; i < Cart.cartItems.length; i++){
+
+    // TODO: Create a TR
+    var tr = document.createElement('tr');
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    var remove = document.createElement('td');
+    remove.textContent = 'Remove';
+    Element.id = 'removeButton';
+    var quant = document.createElement('td');
+    quant.textContent = Cart.cartItems[i].quantity;
+    var itemCell = document.createElement('td');
+    itemCell.textContent = Cart.cartItems[i];
+     
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    tbody.appendChild(tr);
+    tr.appendChild(remove);
+    tr.appendChild(quant);
+    tr.appendChild(itemCell);
+  }
+}
+function whenRemoveClicked(){
 
 }
 
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+    removeButton.onclick;
   // TODO: Save the cart back to local storage
+  
   // TODO: Re-draw the cart table
-
+  clearCart();
+  showCart();
 }
-
+addEventListener('click', removeItemFromCart());
 // This will initialize the page and draw the cart on screen
 renderCart();
